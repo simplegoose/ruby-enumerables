57d2f1ff437614c9c63aa6eb8aea24bce26e9d43
Vanessa O <vaneolipa@gmail.com>
2023-04-06 13:17:53 -0500

    Create a class MyList

---
+++ list.rb
@@ -0,0 +1,32 @@
+require_relative './module'
+
+class MyList
+  include MyEnumerable
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+# <MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+list.all? { |e| e < 5 }
+# true
+list.all? { |e| e > 5 }
+# false
+
+# Test #any?
+list.any? { |e| e == 2 }
+# true
+list.any? { |e| e == 5 }
+# false
+
+# Test #filter
+list.filter(&:even?)
+# [2, 4]
